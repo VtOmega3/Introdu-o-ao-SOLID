b8a1c233d966cca15e8520e4be4e719b2db2a213
Vitor Lobo Muniz <98367740+VtOmega3@users.noreply.github.com>
2022-05-26 11:50:13 -0300

    Add files via upload

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,14 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const newUserAdmin = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(newUserAdmin);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
